3835ee3d41b02a3920f451d5425f9d0c74b778cd
EstherSchiff <esthery248@gmail.com>
2025-05-05 02:04:26 +0000

    add test for stat

---
+++ tests/test_stats.py
@@ -0,0 +1,10 @@
+from stats import game_stats
+from unittest.mock import patch
+import matplotlib
+
+@patch("stats.get_count")
+def test_game_stats(mock_get_count):
+    mock_get_count.return_value = [(5,), (3,)]
+    chart = game_stats()
+    assert isinstance(chart, matplotlib.figure.Figure)
+    mock_get_count.assert_called_once()
